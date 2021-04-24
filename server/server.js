a664db95d8b0f2d8540adb19ba98a82eca739072
arifrumel400 <arif.rumel400@gmail.com>
2021-04-24 23:34:37 +0600

    try to run up server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-//this is arif branch
+// Arif branch 
+
+const app = require('./app')
+
+const dotnev = require('dotenv')
+
+
+
+
+//setting up config file 
+dotenv.config({path: 'server/config/config.env'})
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => console.log(`Server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode `)) 
+const server = app.listen(process.env.PORT, () => console.log(`Server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode `))
