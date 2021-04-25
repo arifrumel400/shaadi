850f60925a4fa8f6d3589b8b68b3a84a003ca895
Mizanur Rahman <nickty.86@gmail.com>
2021-04-25 11:32:12 +0600

    user registration done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose'); 
+require('dotenv').config()
+
+const url = 'mongodb://127.0.0.1:27017/shaadi'
+
+mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log(`MongoDB database connected`)
+} )
+
+app.use(express.json())
+
+const users = require('./routes/user')
+
+
+//app.get('/', (req, res) => console.log("test"))
+
+app.use('/api/', users)
+
+
+app.listen(4000, () => console.log('server is running on port'));
